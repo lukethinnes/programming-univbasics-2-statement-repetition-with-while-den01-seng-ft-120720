c017513b1e1c4b4948b7e4f4c9a2038a238433e1
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-14 03:40:41 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,8 @@
 # Write your code here
-start_num = 10
+count_num = 10
 final_message = "Happy New Year!"
-while start_num>=1 do
-  puts "#{start_num}"
-  start_num -= 1
+while count_num>=1 do
+  puts "#{count_num}"
+  count_num -= 1
 end
 puts final_message
